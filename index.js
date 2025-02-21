36cbdce44f19af64d1b7b3ffee53c375a3d65841
patilsheetal <patilsheetal@patil.local>
2025-02-21 20:37:53 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let heading = document.getElementById('heading'); 
+// <h1 id="heading">sign up</h1>
+console.log(heading.innerHTML); // Outputs the text inside the element
+// sign up
+
+
+let elements = document.getElementsByClassName('signup-container');
+console.log(elements[0]); // Access the first element with the class 'signup-container'
+
+
+for (let i = 0; i < elements.length; i++) {
+    console.log(elements[i].innerHTML);
+}
+
+
+function handleInputChange() {
+    const emailValue = document.getElementById('emailInput').value;
+    console.log(emailValue); // Logs the input value
+}
